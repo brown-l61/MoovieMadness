311550ee2bc760663ce722ac9f16010990db6faf
brown-l61 <brown-l61@ulster.ac.uk>
2023-12-12 15:45:57 +0000

    Updated APIs
    REST APIs created and implemented on both user and admin pages to add, get and delete movies from Blob + Cosmos DB

--- js/app.js
+++ js/app.js
@@ -7,8 +7,17 @@ CLU = "https://prod-33.northeurope.logic.azure.com/workflows/03405edc249b4f49a9e
 
 CUR = "https://prod-23.northeurope.logic.azure.com/workflows/954063f9176d46d782ac572df236b8a9/triggers/manual/paths/invoke/rest/mp/login?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=lY9saNXPzlvPd8Plht7uQ8_NZ0T71lX-KmP_gJSn4FE"
 
-//-- END of URI's --// 
+MUPS = "https://prod-11.northeurope.logic.azure.com/workflows/fa52c681a54647059bb287f6570edd9d/triggers/manual/paths/invoke/rest/mp/movieupload?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=yznuC70r5cjTqX2-u_Sh2IgLupJmw13uVZZQhkLbEnI"//Movie Upload Service
 
+RAM = "https://prod-28.northeurope.logic.azure.com/workflows/219bf55f89d941b0a154d25928d7b90f/triggers/manual/paths/invoke/rest/mp/getmovies?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=px9vG5A_OsTLo8VkzNO73T1OtQuAuZPbmlS5nRf2MGI" //Retrieve all Movies
+
+DIMURI0= "https://prod-36.northeurope.logic.azure.com/workflows/226b2b5288264308bd77b85ebeba7956/triggers/manual/paths/invoke/rest/mp/deletemovie/" //Delete an individual Movie & its metaData (baseURI)
+
+DIMURI1= "?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=UA-6CK7faJ2EfRwATvnk7o--sSv9y3Qby_jQsxRlS8Y" //Delete an individual Movie & its metaData (tailURI)
+
+BLOB_ACCOUNT = "https://mooviemadnessblob.blob.core.windows.net"
+
+//-- END of URI's --// 
 
 //Handlers for button clicks
 $(document).ready(function() {
@@ -29,6 +38,28 @@ $(document).ready(function() {
 		//Execute the userlogin
 		attemptLogin();
 	});
+    
+        $("#getAllMovies").click(function(){
+        //Run the get asset list function
+         getAllMovies();
+    }); 
+    
+
+     $("#getUserMovies").click(function(){
+        //Run the get asset list function
+         getUserMovies();
+    }); 
+    
+    
+   //Handler for the new asset submission button
+    $("#subNewForm").click(function(){
+
+    //Execute the submit new asset function
+    submitNewMovie();
+    
+  }); 
+    
+});  //end of document.ready
 
 
 	//A function to submit a new asset to the REST endpoint //
@@ -139,5 +170,128 @@ $(document).ready(function() {
 				console.error('Error:', status, error);
 			}
 		});
-	}
-}); //end of document.ready
+	}//End of attemptLogin
+    
+    //A function to submit a new asset to the REST endpoint 
+function submitNewMovie(){
+    //Create a form data object
+ submitData = new FormData();
+ //Get form variables and append them to the form data object
+    submitData.append('movieTitle', $('#movieTitle').val());
+ submitData.append('movieGenre', $('#movieGenre').val());
+ submitData.append('movieCertification', $('#movieCertification').val());
+    submitData.append('movieLength', $('#movieLength').val());
+    submitData.append('movieComments', $('#movieComments').val());
+ submitData.append('File', $("#UpFile")[0].files[0]);
+
+
+ //Post the form data to the endpoint, note the need to set the content type header
+ $.ajax({
+ url: MUPS,
+ data: submitData,
+ cache: false,
+ enctype: 'multipart/form-data',
+ contentType: false,
+ processData: false,
+ type: 'POST',
+ success: function(data){    
+     $('#successMessage').text('Successfully added your movie.');      
+ } 
+}); //END of submitNewMovie Function
+}
+
+//A function to get a list of all the assets and write them to the Div with the AssetList Div
+function getAllMovies(){$.getJSON(RAM, function (data) {
+        // Clear the movieList div
+        $('#movieList').empty();
+
+        // Iterate through the returned records and build HTML, incorporating the key values of the record in the data
+        $.each(data, function (key, val) {
+            // Create video element
+            var videoElement = $("<video controls poster='" + BLOB_ACCOUNT + val["filepath"] + "' width='400'/>");
+
+            // Create source element and set its attributes
+            var sourceElement = $("<source/>", {
+                src: BLOB_ACCOUNT + val["filepath"],
+                type: "video/mp4" // adjust the type according to your video format
+            });
+
+            // Append source to video and then append video to movieList
+            videoElement.append(sourceElement);
+            
+              // Create delete button
+            var deleteButton = $("<button>Delete</button>");
+            deleteButton.on("click", function () {
+                // Call the deleteMovie function when the delete button is clicked
+                deleteMovie(val["id"]);
+            });
+            
+            $('#movieList').append("<hr />");
+            $('#movieList').append("Movie: " + val["movieTitle"] + "<br />");
+            $('#movieList').append("Age Certification: " + val["movieCertification"] + "<br />");
+            $('#movieList').append("Genre: " + val["movieGenre"] + "<br />");
+            $('#movieList').append("Run Time (Mins): " + val["movieLength"] + "<br />");
+            $('#movieList').append(videoElement);
+            $('#movieList').append(deleteButton);
+            $('#movieList').append("<hr />");
+            
+            var documentId = val["id"];
+
+        });
+    })
+}//END of getAllMovies Function
+
+
+//A function to delete a movie with a specific ID.
+//The id paramater is provided to the function as defined in the relevant onclick handler
+function deleteMovie(id){
+    $.ajax({
+        type: "DELETE",
+        //Note the need to concatenate the
+        url: DIMURI0 + id + DIMURI1,
+    }).done(function( msg ) {
+        //On success, update the movieList.
+        getAllMovies();
+    });
+} //END deleteMovie() function
+
+
+//Get all movies for user.html 
+function getUserMovies(){$.getJSON(RAM, function (data) {
+        // Clear the movieList div
+        $('#userMovieList').empty();
+
+        // Iterate through the returned records and build HTML, incorporating the key values of the record in the data
+        $.each(data, function (key, val) {
+            // Create video element
+            var videoElement = $("<video controls poster='" + BLOB_ACCOUNT + val["filepath"] + "' width='400'/>");
+
+            // Create source element and set its attributes
+            var sourceElement = $("<source/>", {
+                src: BLOB_ACCOUNT + val["filepath"],
+                type: "video/mp4" // adjust the type according to your video format
+            });
+
+            // Append source to video and then append video to movieList
+            videoElement.append(sourceElement);
+            
+              // Create delete button
+            //var deleteButton = $("<button>Delete</button>");
+            //deleteButton.on("click", function () {
+                // Call the deleteMovie function when the delete button is clicked
+              //  deleteMovie(val["id"]);
+         //   });
+            
+            $('#userMovieList').append("<hr />");
+            $('#userMovieList').append("Movie: " + val["movieTitle"] + "<br />");
+            $('#userMovieList').append("Age Certification: " + val["movieCertification"] + "<br />");
+            $('#userMovieList').append("Genre: " + val["movieGenre"] + "<br />");
+            $('#userMovieList').append("Run Time (Mins): " + val["movieLength"] + "<br />");
+            $('#userMovieList').append(videoElement);
+            $('#userMovieList').append("<hr />");
+            
+            var documentId = val["id"];
+
+        });
+    });
+}//END of getAllMovies Function
